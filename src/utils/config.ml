a73b8b04c24098e80629c0fb556915a6d74cb454
Julian <juliansf@gmail.com>
2013-09-05 15:09:21 +0200

    Some corrections

--- src/utils/config.ml
+++ src/utils/config.ml
@@ -8,6 +8,6 @@ let reg_suffix = ref ".regex"
 
 let print_config oc =
   let p name value = Printf.fprintf oc "%s: %s\n" name value in
-  let p_bool name value = Printf.fprintf oc "%s: %B\n" name value in
+  (* let p_bool name value = Printf.fprintf oc "%s: %B\n" name value in *)
   p "version" version;
   flush oc
